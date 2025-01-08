061715df2458a1bf3b29e325e1f79d294cc0512d
李开广 <likaiguang@songguo7.com>
2025-01-08 20:15:09 +0800

    优化细节

--- open-gray-client/src/main/java/com/gray/lkg/core/service_impl/DefaultGraySwitchClient.java
+++ open-gray-client/src/main/java/com/gray/lkg/core/service_impl/DefaultGraySwitchClient.java
@@ -4,6 +4,7 @@ import com.gray.lkg.client.AbstractGrayPollClient;
 import io.github.persistence.LongPoolConfig;
 
 /**
+ * TODO 通过注解 + JavaAgent 实现自定义静态方法、普通方法的流量拦截
  * Description:
  * Author: 李开广
  * Date: 2024/11/6 11:22 AM
